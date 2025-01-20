181b4f9e5900a2a6cc0c344861365f6d44f34713
AQodS <qoddri@gmail.com>
2025-01-20 18:36:29 +0700

    feat: login

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
 
 type TResgiter = {
   fullName: string;
@@ -10,6 +13,11 @@ type TResgiter = {
   confirmPassword: string;
 };
 
+type Tlogin = {
+  identifier: string;
+  password: string;
+};
+
 const registerValidationSchema = Yup.object({
   fullName: Yup.string().required(),
   username: Yup.string().required(),
@@ -17,7 +25,7 @@ const registerValidationSchema = Yup.object({
   password: Yup.string().required(),
   confirmPassword: Yup.string()
     .required()
-    .oneOf([Yup.ref("password"),""], "Password doesn't match"),
+    .oneOf([Yup.ref("password"), ""], "Password doesn't match"),
 });
 
 export default {
@@ -45,6 +53,76 @@ export default {
         message: "Success registration",
         data: result,
       });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as Tlogin;
+    try {
+      // ambil data user berdasarkan "identifier" -> email / username
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      // validasi password
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "User not found",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "Login success",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "Success get user profile",
+        data: result,
+      })
+
     } catch (error) {
       const err = error as unknown as Error;
       res.status(400).json({
