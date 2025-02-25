ea0a70078d20b79103e2ac10041949613f82ca02
AQodS <qoddri@gmail.com>
2025-02-25 15:34:19 +0700

    feat: middleware control access & api media handling

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ async function init() {
     docs(app);
 
     app.listen(PORT, () => {
-      console.log(`Server is running on port http://localhost:${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.log(error);
