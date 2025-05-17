7911f8b31d148e29c6a2d533c32092a57d01b7b9
AQodS <qoddri@gmail.com>
2025-05-17 07:00:57 +0700

    feat: API findAllByMember & order complete

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,9 +1,15 @@
 import { Response } from "express";
 import { IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
-import OrderModel, { orderDAO, TypeOrder } from "../models/order.model";
+import OrderModel, {
+  orderDAO,
+  OrderStatus,
+  TypeOrder,
+  TypeVoucher,
+} from "../models/order.model";
 import TicketModel from "../models/ticket.model";
 import { FilterQuery } from "mongoose";
+import { getId } from "../utils/id";
 
 export default {
   async create(req: IReqUser, res: Response) {
@@ -90,6 +96,42 @@ export default {
   },
   async findAllByMember(req: IReqUser, res: Response) {
     try {
+      const userId = req.user?.id;
+      const buildQuery = (filter: any) => {
+        let query: FilterQuery<TypeOrder> = {
+          createdBy: userId,
+        };
+
+        if (filter.search) query.$text = { $search: filter.search };
+
+        return query;
+      };
+
+      const { limit = 10, page = 1, search } = req.query;
+
+      const query = buildQuery({
+        search,
+      });
+
+      const result = await OrderModel.find(query)
+        .limit(+limit)
+        .skip((+page - 1) * +limit)
+        .sort({ cratedAt: -1 })
+        .lean()
+        .exec();
+
+      const count = await OrderModel.countDocuments(query);
+
+      response.pagination(
+        res,
+        result,
+        {
+          current: +page,
+          total: count,
+          totalPages: Math.ceil(count / +limit),
+        },
+        "Success find all order by member"
+      );
     } catch (error) {
       response.error(res, error, "Failed to find all order by member");
     }
@@ -97,6 +139,56 @@ export default {
 
   async complete(req: IReqUser, res: Response) {
     try {
+      const { orderId } = req.params;
+      const userid = req.user?.id;
+
+      const order = await OrderModel.findOne({
+        orderId,
+        createdBy: userid,
+      });
+
+      if (!order) return response.notFound(res, "Order not found");
+
+      if (order.status === OrderStatus.COMPLETED)
+        return response.error(res, null, "You have been completed this order");
+
+      const vouchers: TypeVoucher[] = Array.from(
+        { length: order.quantity },
+        () => {
+          return {
+            isPrint: false,
+            voucherId: getId(),
+          } as TypeVoucher;
+        }
+      );
+
+      const result = await OrderModel.findOneAndUpdate(
+        {
+          orderId,
+          createdBy: userid,
+        },
+        {
+          vouchers,
+          status: OrderStatus.COMPLETED,
+        },
+        {
+          new: true,
+        }
+      );
+
+      const ticket = await TicketModel.findById(order.ticket);
+      if (!ticket) return response.notFound(res, "Ticket and order not found");
+
+      await TicketModel.updateOne(
+        {
+          _id: ticket._id,
+        },
+        {
+          quantity: ticket.quantity - order.quantity,
+        }
+      );
+
+      response.success(res, result, "Success complete an order");
     } catch (error) {
       response.error(res, error, "Failed to complete an order");
     }
