249889e4b75772f5121490e50353fea0d5e7f011
AQodS <qoddri@gmail.com>
2025-09-01 07:53:38 +0700

    refactor: API auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,67 +1,16 @@
 import { Request, Response } from "express";
-import * as Yup from "yup";
-import UserModel from "../models/user.model";
+import UserModel, { userDTO, userLoginDTO } from "../models/user.model";
 import { encrypt } from "../utils/encryption";
 import { generateToken } from "../utils/jwt";
 import { IReqUser } from "../utils/interfaces";
 import response from "../utils/response";
 
-type TResgiter = {
-  fullName: string;
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
-
-type Tlogin = {
-  identifier: string;
-  password: string;
-};
-
-const registerValidationSchema = Yup.object({
-  fullName: Yup.string().required(),
-  username: Yup.string().required(),
-  email: Yup.string().required(),
-  password: Yup.string().required()
-    .min(6, "Password must be at least 6 characters")
-    .test(
-      "at-least-one-uppercase-letter",
-      "contains at least one uppercase letter",
-      (value) => {
-        if (!value) return false;
-        const regex = /^(?=.*[A-Z])/;
-        return regex.test(value);
-      }
-    )
-    .test(
-      "at-least-one-number", 
-      "contains at least one number", 
-      (value) => {
-        if (!value) return false;
-        const regex = /^(?=.*\d)/;
-        return regex.test(value);
-      }
-    ),
-  confirmPassword: Yup.string()
-    .required()
-    .oneOf([Yup.ref("password"), ""], "Password doesn't match"),
-});
-
 export default {
   async register(req: Request, res: Response) {
-    /**
-    #swagger.tags = ['Auth']
-    #swagger.requestBody = {
-      required: true,
-      schema: {$ref: "#/components/schemas/RegisterRequest"}
-    }
-     */
-    const { fullName, username, email, password, confirmPassword } =
-      req.body as unknown as TResgiter;
+    const { fullName, username, email, password, confirmPassword } = req.body;
 
     try {
-      await registerValidationSchema.validate({
+      await userDTO.validate({
         fullName,
         username,
         email,
@@ -83,16 +32,14 @@ export default {
   },
 
   async login(req: Request, res: Response) {
-    /**
-    #swagger.tags = ['Auth']
-    #swagger.requestBody = {
-      required: true,
-      schema: {$ref: "#/components/schemas/LoginRequest"}
-    } 
-     */
-    const { identifier, password } = req.body as unknown as Tlogin;
+    const { identifier, password } = req.body;
     try {
       // ambil data user berdasarkan "identifier" -> email / username
+      await userLoginDTO.validate({
+        identifier,
+        password,
+      });
+
       const userByIdentifier = await UserModel.findOne({
         $or: [
           {
@@ -129,12 +76,6 @@ export default {
   },
 
   async me(req: IReqUser, res: Response) {
-    /**
-    #swagger.tags = ['Auth']
-    #swagger.security = [{
-      "bearerAuth": []
-    }]
-     */
     try {
       const user = req.user;
       const result = await UserModel.findById(user?.id);
@@ -146,13 +87,6 @@ export default {
   },
 
   async activation(req: Request, res: Response) {
-    /**
-    #swagger.tags = ['Auth']
-    #swagger.requestBody = {
-      required: true,
-      schema: {$ref: "#/components/schemas/ActivationRequest"}
-    }
-     */
     try {
       const { code } = req.body as { code: string };
       const user = await UserModel.findOneAndUpdate(
